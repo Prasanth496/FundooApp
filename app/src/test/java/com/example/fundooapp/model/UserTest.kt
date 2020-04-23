8d798c5adae4664a9004f2fcb145ea7f3b0f46ce
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-04-23 15:49:13 +0530

    [prasanth]refactor. created testcase for reference check

--- app/src/test/java/com/example/fundooapp/model/UserTest.kt
+++ app/src/test/java/com/example/fundooapp/model/UserTest.kt
@@ -5,12 +5,19 @@ import org.junit.Test
 
 class UserTest {
 
-    // function for null check
     @Test
+    // function for null check
     fun givenUserOneAndNull_WhenEquals_ShouldNotBeEqual() {
         val userOne = Userlogin(email = "pavan123@gmail.com", password = "pavan@123")
         val userTwo = null
         Assert.assertFalse(userOne == userTwo)
+    }
+
+    @Test
+    // function for reference check
+    fun givenSameUser_WhenEquals_ShouldBeEqual() {
+        val userOne = Userlogin(email = "pavan123@gmail.com",password = "pavan@123")
+        Assert.assertTrue(userOne.equals(userOne))
     }
 
 
