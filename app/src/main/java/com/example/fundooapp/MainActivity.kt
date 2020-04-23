4ba104b5ad0b56f351168a1e56123f90977e6c2b
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-04-23 14:24:44 +0530

    [prasanth]add. created layout for splashscreen

--- app/src/main/java/com/example/fundooapp/MainActivity.kt
+++ app/src/main/java/com/example/fundooapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fundooapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val background = object : Thread(){
+            override fun run(){
+                try {
+                    Thread.sleep(3000)
+                    val intent = Intent(baseContext,HomeActivity::class.java)
+                    startActivity(intent)
+                }
+                catch(e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
