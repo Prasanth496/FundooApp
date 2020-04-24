a0347b3a55688a2a722e4aabceba271d49a6ce31
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-04-24 15:33:03 +0530

    [prasanth]refactor. created testcase when password length is less than three  for RegisterModel

--- app/src/main/java/com/example/fundooapp/model/RegisterModel.kt
+++ app/src/main/java/com/example/fundooapp/model/RegisterModel.kt
@@ -1,4 +1,6 @@
 package com.example.fundooapp.model
 
-class RegisterModel (val firstname : String,val lastname : String,val username : String, val password : String){
+class RegisterModel (val firstname : String,val lastname : String,val username : String, val password : String) {
+
+
 }
--- app/src/test/java/com/example/fundooapp/model/RegisterTest.kt
+++ app/src/test/java/com/example/fundooapp/model/RegisterTest.kt
@@ -27,4 +27,12 @@ class RegisterTest {
         val userTwo = RegisterModel(firstname = "Ravi",lastname = "Krishna",username = "ravi123@gmail.com",password="ravi@123")
         Assert.assertTrue(userOne.equals(userOne))
     }
+
+    @Test
+    // function when password length is less than three
+    fun givenUserOneAndUserTwo_WhenPasswordLengthLessThanThree_ShouldNotBeEqual() {
+        val userOne = RegisterModel(firstname = "Pavan",lastname = "Kumar",username = "pavan123@gmail.com",password="12")
+        val userTwo = RegisterModel(firstname = "Ravi",lastname = "Krishna",username = "ravi123@gmail.com",password="34")
+        Assert.assertFalse(userOne.equals(userTwo))
+    }
 }
